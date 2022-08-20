f655bf334e97bc69a0a29c0cff14b327dc0c66bf
Conrado Costa <conradocostaad@yahoo.com.br>
2022-08-20 20:40:07 -0300

    Creating shipping prices component and using http client to get data

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 
 @Injectable({
@@ -9,7 +9,9 @@ export class CartService {
 
   items: Product[] = [];
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
 
   addTocart(prodcut: Product) {
     this.items.push(prodcut);
@@ -22,6 +24,10 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.httpClient.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
   
 }
